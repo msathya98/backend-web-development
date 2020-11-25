8a436bdeb1470f2a3647f576a74b53fe60f353ac
Sathya M <62427113+sathya-sjce@users.noreply.github.com>
2020-11-25 13:04:26 +0530

    Basic example of using EJS

---
+++ EJS/app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+//app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+	var today = new Date();
+	var currentDay = today.getDay();
+    var day = "";
+
+	if (currentDay === 0 ){
+		day = "Sunday";
+	} else if (currentDay === 1){
+		day = "Monday";
+	} else if (currentDay === 2 ){
+		day = "Tuesday";
+		} else if (currentDay === 3 ){
+		day = "Wednesday";
+		} else if (currentDay === 4 ){
+		day = "Thursday";
+							
+			} else if (currentDay === 5 ){
+		day = "Friday";
+		} else if (currentDay === 6 ){
+		day = "Saturday";
+							
+	}; //we can ALSO use switch statement here
+
+res.render("list", {KindOfDay: day});
+
+});
+
+
+app.listen(3000, function(){
+	console.log("server started");
+});
